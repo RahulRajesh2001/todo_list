554547375221c4c84843953b736c88305a3abf98
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2023-04-21 15:12:15 +0530

    todo list application. that contains add functionality , delete functionality etc. It was created in react without configure database

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import "./App.css"
 
 function App() {
+  const[toDos,setTodos]=useState([])
+  const[todo,setTodo]=useState('')
+
+//to delete the todos when the delete button is clicked
+const handleDelete = (id) => {
+  setTodos(toDos.filter((obj) => obj.id !== id));
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Hey... It's {Date.now()} </h2>
+      </div>
+      <div className="input">
+        <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,{id:Date.now() ,text:todo , status:false}])} className="fas fa-plus"></i>
+      </div>
+      
+      <div className="todos">
+      {toDos.map((obj)=>{
+      //map function for iterating the todos array
+    return(
+      <div className="todo">
+            <div className="left">
+              <input onChange={(e)=>{
+                setTodos(toDos.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                    obj2.status=e.target.checked
+                    
+                  }
+                  return obj2
+                }))
+                
+              }} value={obj.status} type="checkbox" name="" id="" />
+             <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i onClick={()=>handleDelete(obj.id)} className="fas fa-times"></i>
+            </div>
+          </div>
+      )
+})}
+     
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
+
